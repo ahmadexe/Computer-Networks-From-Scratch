e23c88b1e6645dd783253388398e5b32052fb7be
Ahmad <muahmad710@gmail.com>
2023-01-12 21:03:15 +0500

    Basic source code for computer networks

---
+++ Sockets/serve.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 55555))
+s.listen()
+
+while True:
+    client, address = s.accept()
+    print("Connected to: {}".format(str(address)))
+    client.send("You're connected".encode())
+    client.close()
+          
